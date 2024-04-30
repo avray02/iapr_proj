685e96129f44957d3e6f2eab4eda2d970b8df486
Alexandre Vray <alexandre.vray@epfl.ch>
2024-04-30 20:36:24 +0200

    display function

---
+++ utils/display.py
@@ -0,0 +1,40 @@
+import numpy as np 
+import matplotlib.pyplot as plt 
+
+def plot_images(images, coins, estimate_labels=[], n_cols=1, x_size=3, y_size=3, cmap='gray'):  
+    
+    # i = 0
+    labels = ['5CHF', '2CHF', '1CHF', '0.5CHF', '0.2CHF', '0.1CHF', '0.05CHF',
+       '2EUR', '1EUR', '0.5EUR', '0.2EUR', '0.1EUR', '0.05EUR', '0.02EUR',
+       '0.01EUR', 'OOD']
+    
+    n_images = len(images)
+    n_rows = int(np.ceil(n_images/n_cols))
+
+    _, axs = plt.subplots(n_rows, n_cols, figsize=(n_cols * x_size, n_rows * y_size))
+
+    for i, (image, coin) in enumerate(zip(images, coins)):
+        row = i // n_cols
+        col = i % n_cols
+        if n_rows > 1:
+            ax = axs[row, col]
+        else:
+            ax = axs[col]
+
+        ax.imshow(image, cmap=cmap)
+        ax.axis('off')
+
+        text = ''
+        for j in range(len(labels)):
+            if coins[i][j] != 0:
+                text += f'{coins[i][j]}x{labels[j]}\n'
+
+        ax.text(0, 0, text, color='white', fontsize=10, va='top', ha='left', bbox=dict(facecolor='black', alpha=0.2))
+        # ax.set_title(coin)
+
+    plt.show()
+    
+    # plt.imshow(images[i])
+    # plt.show()
+
+    
